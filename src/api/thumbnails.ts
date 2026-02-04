331fd7108fa24786d7f9f04c2fafc428c0c5084c
farulivan <farul@nabitu.id>
2026-02-04 10:39:32 +0700

    feat: handle uploading thumbnails

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
+import { getInMemoryURL } from "./assets";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -45,9 +46,46 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const formData = await req.formData();
+  const thumbnail = formData.get("thumbnail");
+  if (!(thumbnail instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
 
-  // TODO: implement the upload here
+  const MAX_UPLOAD_SIZE = 10 << 20;
 
-  return respondWithJSON(200, null);
+  if (thumbnail.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      "Thumbnail file exceeds the maximum allowed size of 10MB",
+    );
+  }
+
+  const mediaType = thumbnail.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  const thumbnailData = await thumbnail.arrayBuffer();
+  if (!thumbnailData) {
+    throw new Error("Error reading file data");
+  }
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Video not found");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Forbidden access to the video");
+  }
+
+  videoThumbnails.set(videoId, {
+    data: thumbnailData,
+    mediaType,
+  });
+
+  const urlPath = getInMemoryURL(cfg, videoId);
+  video.thumbnailURL = urlPath;
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
