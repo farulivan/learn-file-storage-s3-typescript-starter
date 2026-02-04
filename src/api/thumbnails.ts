dbb4b074b44ca9a895ee86fcfe15550804700429
farulivan <farul@nabitu.id>
2026-02-04 12:33:07 +0700

    feat: update uploading thumbnails to local storage

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,41 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
-import { getInMemoryURL } from "./assets";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { getAssetDiskPath, getAssetUrl, mediaTypeToExt } from './assets';
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -61,13 +30,8 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   }
 
   const mediaType = thumbnail.type;
-  if (!mediaType) {
-    throw new BadRequestError("Missing Content-Type for thumbnail");
-  }
-
-  const thumbnailData = await thumbnail.arrayBuffer();
-  if (!thumbnailData) {
-    throw new Error("Error reading file data");
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid file type. Only JPEG or PNG allowed.");
   }
 
   const video = getVideo(cfg.db, videoId);
@@ -78,13 +42,15 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError("Forbidden access to the video");
   }
 
-  videoThumbnails.set(videoId, {
-    data: thumbnailData,
-    mediaType,
-  });
+  const ext = mediaTypeToExt(mediaType);
+  const filename = `${videoId}${ext}`;
 
-  const urlPath = getInMemoryURL(cfg, videoId);
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, thumbnail);
+
+  const urlPath = getAssetUrl(cfg, filename);
   video.thumbnailURL = urlPath;
+
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
